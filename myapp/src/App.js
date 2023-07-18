5156416d8c64a84771ab0fd1e01a5b9d08de40ca
satishchaduvula <satishchaduvula@gmail.com>
2023-07-18 13:43:10 +0530

    first commot
    Signed-off-by: satishchaduvula <satishchaduvula@gmail.com>

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import FoodPage from "./Components/homefood";
+
+function App() {
+  return (
+    <div className="App">
+      <FoodPage />
+    </div>
+  );
+}
+
+export default App;
