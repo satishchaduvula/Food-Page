f1de74fc1465980684f8a2c8ace156690ddcaab9
satishchaduvula <satishchaduvula@gmail.com>
2023-08-04 11:09:21 +0530

    message
    Signed-off-by: satishchaduvula <satishchaduvula@gmail.com>

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,13 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import FoodPage from "./Components/homefood";
+// import FoodPage from "./Components/homefood";
+import Card from "./Components/card";
 
 function App() {
   return (
     <div className="App">
-      <FoodPage />
+      {/* <FoodPage /> */}
+      <Card />
     </div>
   );
 }
